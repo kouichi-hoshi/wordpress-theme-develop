13edb31be83e405b5fd91892f9effea2c4ec7ff8
kouichi hoshi <kouichi.hoshi@stella-design.biz>
2020-05-16 16:24:15 +0900

    初回コミット

---
+++ bs-config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "proxy": "http://localhost:8888/wordpress-theme-develop/dist/" //MAMP等のローカルサーバーを指定
+};
---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve  from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel  from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/assets/js/script.js',
+  output: {
+    format: 'iife',
+    dir: 'dist/wp-content/themes/wordpress-theme-develop' //テーマファイルを出力したいパスを指定
+  },
+  plugins: [
+    resolve({
+      jsnext: true
+    }),
+    commonjs(),
+    babel({
+      presets: [
+        [
+          "@babel/preset-env", {
+          "modules": false,
+          "targets": {
+            "browsers": ['last 2 versions']
+          }
+        }
+        ]
+      ],
+      babelrc: false
+    })
+  ],
+  experimentalCodeSplitting: true
+};
